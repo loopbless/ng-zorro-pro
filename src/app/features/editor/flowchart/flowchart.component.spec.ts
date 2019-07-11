dfbe059e7c3f17184d5b034e66a9e625be7882ce
hejialin <hejialin@veredholdings.com>
2019-07-11 17:37:20 +0800

    fix: build ssr error

--- src/app/features/editor/flowchart/flowchart.component.spec.ts
+++ src/app/features/editor/flowchart/flowchart.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FlowchartComponent } from './flowchart.component';
+import { NzoFlowchartComponent } from './flowchart.component';
 
-describe('FlowchartComponent', () => {
-  let component: FlowchartComponent;
-  let fixture: ComponentFixture<FlowchartComponent>;
+describe('NzoFlowchartComponent', () => {
+  let component: NzoFlowchartComponent;
+  let fixture: ComponentFixture<NzoFlowchartComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FlowchartComponent ]
+      declarations: [ NzoFlowchartComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FlowchartComponent);
+    fixture = TestBed.createComponent(NzoFlowchartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
